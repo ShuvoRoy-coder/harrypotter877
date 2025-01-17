7a5d538127a5947c0165eda879bcb36433150209
ShuvoRoy-coder <sroy90677@gmail.com>
2025-01-17 22:18:46 +0600

    Updated

---
+++ assets/js/tailwind.js
@@ -0,0 +1,23 @@
+
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                blue1: '#2bbced',
+                blue2: '#1058A2',
+            },
+            fontFamily: {
+                roboto: ['Roboto', 'serif'],
+                gothamBold: ['GothamBold', 'sans-serif'],
+                gothamBook: ['GothamBook', 'sans-serif'],
+                gothamMedium: ['GothamMedium', 'sans-serif'],
+            },
+            screens: {
+                mini: '450px',
+            },
+            container: {
+                center: true,
+            },
+        }
+    }
+}
